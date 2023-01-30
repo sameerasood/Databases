3ecc36a047e241b77bfdae91686d7d83a7965985
sameera sood <sameerasood@gmail.com>
2023-01-30 22:24:07 +0000

    Phase-3 Bookstore project setup

---
+++ Phase-2/music_library/app.rb
@@ -0,0 +1,12 @@
+require_relative 'lib/database_connection'
+require_relative 'lib/artist_repository'
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect('music_library')
+
+# Perform a SQL query on the database and get the result set.
+artist_repository = ArtistRepository.new
+
+artist_repository.all.each do |artist|
+  p artist
+end
